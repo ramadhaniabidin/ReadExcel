d566097919a21811beba09d94a07532e8abeb864
ramadhaniabidin12@gmail.com <ramadhaniabidin12@gmail.com>
2023-08-01 11:10:43 +0700

    test

--- ReadExcel/Program.cs
+++ ReadExcel/Program.cs
@@ -1,7 +1,14 @@
-﻿public class Program
+﻿using ReadExcel;
+using System.Data;
+
+public class Program
 {
     public static void Main()
     {
         Console.WriteLine("Hello World");
+        ExcelManager excelManager = new ExcelManager();
+        DataTable dt = excelManager.ExcelRead("C:\\Users\\ramad\\Downloads\\test.xlsx");
+
+        Console.WriteLine(dt);
     }
 }
