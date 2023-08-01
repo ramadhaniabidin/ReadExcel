6ca36ee298a16774099ed891e016f37c9fb38559
ramadhaniabidin12@gmail.com <ramadhaniabidin12@gmail.com>
2023-08-01 10:51:04 +0700

    instal package ClosedXML

---
+++ ReadExcel/ExcelManager.cs
@@ -0,0 +1,26 @@
+﻿using ClosedXML.Excel;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReadExcel
+{
+    public class ExcelManager
+    {
+        public DataTable ExcelRead(string Path)
+        {
+            //Save the uploaded Excel file.
+            //string filePath = Server.MapPath("~/Files/") + Path.GetFileName(FileUpload1.PostedFile.FileName);
+            //FileUpload1.SaveAs(filePath);
+
+            //Open the Excel file using ClosedXML.
+            using(XLWorkbook workbook = new XLWorkbook(Path))
+            {
+
+            }
+        }
+    }
+}
