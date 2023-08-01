fd20bc5a66b370d298f08329eb353cad299d8a83
ramadhaniabidin <ramadhaniabidin12@gmail.com>
2023-08-01 02:53:55 -0700

    Successfully insert data to database but not using stored procedure

---
+++ ReadExcel/DatabaseManager.cs
@@ -0,0 +1,218 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReadExcel
+{
+    public class DatabaseManager
+    {
+        public string connString = "Data Source=(localdb)\\local;Initial Catalog=ExcelTest;Integrated Security=True;";
+        public SqlCommand cmd;
+        public SqlDataReader dReader;
+        public SqlTransaction trans;
+
+
+        public void OpenConnection(ref SqlConnection connection, bool isTrans = true)
+        {
+            if(connection == null)
+            {
+                connection = new SqlConnection(connString);
+                connection.Open();
+                cmd = connection.CreateCommand();
+                cmd.CommandTimeout = 0;
+                if (isTrans)
+                {
+                    trans = connection.BeginTransaction();
+                    cmd.Transaction = trans;
+                }
+            }
+
+            else
+            {
+                if (connection.State == ConnectionState.Closed)
+                {
+                    connection = new SqlConnection(connString);
+                    connection.Open();
+                    cmd = connection.CreateCommand();
+                    cmd.CommandTimeout = 0;
+                    if (isTrans)
+                    {
+                        trans = connection.BeginTransaction();
+                        cmd.Transaction = trans;
+                    }
+                }
+            }
+        }
+
+
+        public void CloseConnection(ref SqlConnection connection, bool isTrans = false)
+        {
+            if (connection?.State == ConnectionState.Open)
+            {
+                if (isTrans)
+                {
+                    trans.Commit();
+                }
+                connection?.Close();
+            }
+            connection?.Dispose();
+            cmd?.Dispose();
+        }
+
+        public void AddInParameter(SqlCommand command, string name, object value)
+        {
+            SqlParameter parameter = new SqlParameter();
+            parameter.ParameterName = name;
+            parameter.Value = value;
+            parameter.Direction = ParameterDirection.Input;
+
+            command.Parameters.Add(parameter);
+        }
+
+        public void AddInParameter(SqlCommand command, string name, SqlDbType type)
+        {
+            SqlParameter parameter = new SqlParameter();
+            parameter.ParameterName = name;
+            parameter.SqlDbType = type;
+            parameter.Direction = ParameterDirection.Output;
+
+            command.Parameters.Add(parameter);
+        }
+
+        public void CloseDataReader(SqlDataReader dataReader)
+        {
+            if (dataReader == null)
+            {
+                return;
+            }
+            dataReader.Close();
+            dataReader.Dispose();
+        }
+
+        public DataTable GetDataByProcedure(string SPName, string ParamName, string ParamValue)
+        {
+            DataTable dt = new DataTable();
+            SqlDataReader dr = null;
+            cmd.CommandText = SPName;
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.Clear();
+            cmd.Parameters.AddWithValue(ParamName, ParamValue);
+            dr = cmd.ExecuteReader();
+            dt.Load(dr);
+            CloseDataReader(dr);
+            return dt;
+        }
+
+        public bool isRecordExists(string SPName, string ParamName, string ParamValue)
+        {
+            SqlDataReader dr = null;
+            cmd.CommandText = SPName;
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.Clear();
+            cmd.Parameters.AddWithValue(ParamName, ParamValue);
+            dr = cmd.ExecuteReader();
+            if (dr.HasRows)
+            {
+                CloseDataReader(dr);
+                return true;
+            }
+            else
+            {
+                CloseDataReader(dr);
+                return false;
+            }
+        }
+
+        public bool isRecordExists(string query)
+        {
+            SqlDataReader dr = null;
+            cmd.CommandText = query;
+            cmd.CommandType = CommandType.StoredProcedure;
+            dr = cmd.ExecuteReader();
+            if (dr.HasRows)
+            {
+                CloseDataReader(dr);
+                return true;
+            }
+            else
+            {
+                CloseDataReader(dr);
+                return false;
+            }
+        }
+
+
+        public string AutoCounter(string fieldName, string TableName, string fieldCriteria, string valueCriteria, int LengthOfString, string fieldNameConverted = "")
+        {
+            SqlDataReader dataReader;
+            string str = "";
+            string str2 = "1";
+
+            cmd.CommandText = $"SELECT TOP 1 {fieldName} FROM {TableName} WHERE {fieldCriteria} LIKE '%{valueCriteria}%'";
+            if (fieldNameConverted != "")
+            {
+                cmd.CommandText += $" ORDER BY {fieldNameConverted} DESC";
+            }
+            else
+            {
+                cmd.CommandText += $" ORDER BY {fieldName} DESC";
+            }
+            cmd.CommandType = CommandType.Text;
+            dReader = cmd.ExecuteReader();
+
+            if (dReader.HasRows)
+            {
+                dReader.Read();
+                str2 = dReader[fieldName].ToString();
+
+                int StartFrom = valueCriteria.Length;
+                int EndUntil = 0;
+                EndUntil = str2.Length - StartFrom;
+
+                str2 = (int.Parse(str2.Substring(StartFrom, EndUntil)) + 1).ToString();
+            }
+
+            int i = 0;
+            while (i < LengthOfString - str2.Length)
+            {
+                str += "0";
+                i += 1;
+            }
+            str = valueCriteria + str + str2;
+            CloseDataReader(dReader);
+            return str;
+        }
+
+        public string GetValueFromQuery(string Query, string FieldName)
+        {
+            string result = "";
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = Query;
+            dReader = cmd.ExecuteReader();
+            while (dReader.Read())
+            {
+                result = dReader[FieldName].ToString();
+            }
+            CloseDataReader(dReader);
+            return result;
+        }
+
+        public DataTable GetValueFromSP(string SPName, string ParamName, string ParamValue)
+        {
+            DataTable dt = new DataTable();
+            cmd.CommandText = SPName;
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.Clear();
+            AddInParameter(cmd, ParamName, ParamValue);
+            dReader = cmd.ExecuteReader();
+            dt.Load(dReader);
+            CloseDataReader(dReader);
+            return dt;
+        }
+
+    }
+}
--- ReadExcel/Program.cs
+++ ReadExcel/Program.cs
@@ -1,20 +1,27 @@
 ﻿using ReadExcel;
 using System.Data;
+using System.Data.SqlClient;
 
 public class Program
 {
+    public string connString = "Data Source=(localdb)\\local;Initial Catalog=ExcelTest;Integrated Security=True;";
+    DatabaseManager db = new DatabaseManager();
+    SqlConnection conn = new SqlConnection();
+
+
     public static void Main()
     {
         string Path = "C:\\Users\\Bidin\\Downloads\\test.xlsx";
 
         Console.WriteLine("Hello World");
         ExcelManager excelManager = new ExcelManager();
+        DatabaseManager databaseManager = new DatabaseManager();
         DataTable dt = new DataTable();
 
         dt = excelManager.ExcelRead(Path);
         Console.WriteLine("Column Names:");
 
-        List<string> Columns = dt.Columns.Cast<DataColumn>().Select(col => col.ColumnName).ToList();
+        List<string> Columns = dt.Columns.Cast<DataColumn>().Select(col => col.ColumnName.Replace(" ", "_")).ToList();
         List<string> Rows = dt.Rows.Cast<DataRow>().Select(row => string.Join("\t", row.ItemArray)).ToList();
 
         Console.WriteLine($"Column count: {Columns.Count}");
@@ -22,27 +29,27 @@ public class Program
 
 
         string query = $"INSERT INTO TABLE (\n{string.Join(",\n", Columns)}" + "\n)";
-        Console.WriteLine(query);
+        //Console.WriteLine(query);
 
         string value = "\n\nVALUES \n";
 
-        Console.WriteLine("VALUES");
+        //Console.WriteLine("VALUES");
         for (int i = 0; i < Rows.Count; i++)
         {
-            Console.Write("(");
+            //Console.Write("(");
             value += "(";
             for (int j = 0; j < Columns.Count; j++)
             {
-                value += $"'{dt.Rows[i][j]},'";
-
                 if(j == Columns.Count - 1)
                 {
-                    Console.Write($"'{dt.Rows[i][j]}'");
+                    value += $"'{dt.Rows[i][j]}'";
+                    //Console.Write($"'{dt.Rows[i][j]}'");
                 }
 
                 else
                 {
-                    Console.Write($"'{dt.Rows[i][j]}',");
+                    value += $"'{dt.Rows[i][j]}',";
+                    //Console.Write($"'{dt.Rows[i][j]}',");
                 }
                 
             }
@@ -50,16 +57,116 @@ public class Program
             if(i == Rows.Count - 1)
             {
                 value += ")\n\n";
-                Console.WriteLine(")\n");
+                //Console.WriteLine(")\n");
             }
 
             else
             {
                 value += "),\n\n";
-                Console.WriteLine("),\n");
+                //Console.WriteLine("),\n");
             }
            
         }
+
         Console.WriteLine($"\nThe amount of sheet: { excelManager.Test(Path)}");
+
+        InsertFromQuery();
+    }
+
+
+    public void InsertFromSP()
+    {
+        ExcelManager excelManager = new ExcelManager();
+        DataTable dt = new DataTable();
+        string Path = "C:\\Users\\Bidin\\Downloads\\test.xlsx";
+        dt = excelManager.ExcelRead(Path);
+
+        List<string> Columns = dt.Columns.Cast<DataColumn>().Select(col => col.ColumnName).ToList();
+        List<string> Rows = dt.Rows.Cast<DataRow>().Select(row => string.Join("\t", row.ItemArray)).ToList();
+
+        db.OpenConnection(ref conn);
+        for(int i = 0; i < Rows.Count; i++)
+        {
+            db.cmd.CommandText = "dbo.Insert_Entity_Table";
+            db.cmd.CommandType = CommandType.StoredProcedure;
+            db.cmd.Parameters.Clear();
+            for (int j = 0; j < Columns.Count; j++)
+            {
+
+            }
+        }
+
+
+
+
+
+    }
+
+    public static void InsertFromQuery()
+    {
+        ExcelManager excelManager = new ExcelManager();
+        DataTable dt = new DataTable();
+        string Path = "C:\\Users\\Bidin\\Downloads\\test.xlsx";
+        dt = excelManager.ExcelRead(Path);
+
+        List<string> Columns = dt.Columns.Cast<DataColumn>().Select(col => col.ColumnName.Replace(" ", "_")).ToList();
+        List<string> Rows = dt.Rows.Cast<DataRow>().Select(row => string.Join("\t", row.ItemArray)).ToList();
+        string connString = "Data Source=(localdb)\\local;Initial Catalog=ExcelTest;Integrated Security=True;";
+
+        string query = $"INSERT INTO dbo.ENTITAS (\n{string.Join(",\n", Columns)}" + "\n)";
+        string value = "\n\nVALUES \n";
+
+        for (int i = 0; i < Rows.Count; i++)
+        {
+            value += "(";
+            for (int j = 0; j < Columns.Count; j++)
+            {
+
+
+                if (j == Columns.Count - 1)
+                {
+                    value += $"'{dt.Rows[i][j]}'";
+                }
+
+                else
+                {
+                    value += $"'{dt.Rows[i][j]}',";
+                }
+
+            }
+
+            if (i == Rows.Count - 1)
+            {
+                value += ")\n\n";
+            }
+
+            else
+            {
+                value += "),\n\n";
+            }
+
+        }
+
+        Console.WriteLine(query + value);
+
+        try
+        {
+            using (SqlConnection conn = new SqlConnection(connString))
+            {
+                conn.Open();
+
+
+                using (SqlCommand cmd = new SqlCommand((query + value), conn))
+                {
+                    cmd.ExecuteNonQuery();
+                }
+                conn.Close();
+            }
+            Console.WriteLine("Data inserted successfully.");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Error: " + ex.Message);
+        }
     }
 }
